2d6581737897debb13ad0d1da1dc8d12c1ae7c69
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-11 13:19:16 -0600

    Customer: added association

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
+  has_many :orders
+  has_many :comments
+
   validates :first_name, :last_name, :email, :username, :password, :address, :phone_number, :user_role, presence: true
   enum user_role: { admin: 1, registered_user: 2, visitor: 3 }
 end
