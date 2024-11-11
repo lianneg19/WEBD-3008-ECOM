cb70af0986d7d54aa3d7a56355dcf55934866736
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-11 13:59:07 -0600

    Product: added validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :order_products
   has_many :orders, through: :order_products
+
+  validates :name, :descriptio, :price, presence: true
+  validates :price, :decimal, precision: 5, scale: 2, numericality: { greater_than: 0 }
 end
