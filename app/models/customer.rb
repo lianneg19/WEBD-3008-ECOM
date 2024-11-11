ca5629f62f195ea4abfb9b9341646a568ca8133e
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-11 13:15:25 -0600

    Customer: addded validations, enum for user_role

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,4 @@
 class Customer < ApplicationRecord
+  validates :first_name, :last_name, :email, :username, :password, :address, :phone_number, :user_role, presence: true
+  enum user_role: { admin: 1, registered_user: 2, visitor: 3 }
 end
