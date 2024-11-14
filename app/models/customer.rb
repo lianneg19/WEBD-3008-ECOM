cac631512e45bf682e09beeba79bf3ef8b3cf0b3
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-14 14:34:02 -0600

    removed the validation presence for email, username, and password

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,6 @@ class Customer < ApplicationRecord
   has_many :orders
   has_many :comments
 
-  validates :first_name, :last_name, :email, :username, :password, :address, :phone_number, :user_role, presence: true
+  validates :first_name, :last_name, :address, :phone_number, :user_role, presence: true
   enum user_role: { admin: 1, registered_user: 2, visitor: 3 }
 end
