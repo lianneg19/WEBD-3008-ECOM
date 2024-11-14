1e54cfad66dae79d394ca636b30d67f69259f32e
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-14 13:58:06 -0600

    Removed category description

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_11_185908) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_14_022842) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
