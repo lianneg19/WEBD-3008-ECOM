84c416237db928dd3fd8d738f6bdd37333220923
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-20 11:41:01 -0600

    removed before action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,18 +1,13 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[ show edit update destroy ]
-
-  # GET /categories or /categories.json
   def index
     @categories = Category.all
   end
 
-  # GET /categories/1 or /categories/1.json
   def show
     @category = Category.find(params[:id])
   end
 
   private
-    # Only allow a list of trusted parameters through.
     def category_params
       params.require(:category).permit(:name, :description)
     end
