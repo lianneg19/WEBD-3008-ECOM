c9b962745cd293b1550f80dc17cc3d62047f94b8
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-20 15:13:59 -0600

    added new column named status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_15_033901) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_20_211100) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_15_033901) do
     t.integer "customer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
