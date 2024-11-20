3f1e8650d379e967db2cf0c0febfe2f4fdbdbc81
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-20 15:16:07 -0600

    added validation and enum to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,7 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_products
   has_many :products, through: :order_products
+
+  validates :status, presence: true
+  enum status: { active: 1, shipped: 2, canceled: 3 }
 end
