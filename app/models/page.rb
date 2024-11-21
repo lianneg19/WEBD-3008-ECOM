ef52b3ab34b4dfde6f7c14f44a8c867f945e44d6
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-21 13:05:12 -0600

    uncomment permit params, added ransackable attr, and validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,7 @@
 class Page < ApplicationRecord
+  validates :title, :content, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "content", "created_at", "id", "title", "updated_at" ]
+  end
 end
