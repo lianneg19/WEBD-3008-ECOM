a780baa26b11e810b6f5ef496fc98e8b03c9e53d
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-20 23:31:37 -0600

    added call to method for search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   # GET /products or /products.json
   def index
-    @products = Product.page(params[:page]).per(20)
+    @products = Product.search(params[:search]).page(params[:page]).per(20)
   end
 
   # GET /products/1 or /products/1.json
