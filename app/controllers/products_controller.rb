b2acb455d9930cd09735f6a8222a9ecd5f0f46f2
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-21 01:32:28 -0600

    fixed the search by changing parameter to the correct one, category id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   # GET /products or /products.json
   def index
-    @products = Product.search(params[:search], params[:search]).page(params[:page]).per(20)
+    @products = Product.search(params[:search], params[:category_id]).page(params[:page]).per(20)
     @categories = Category.all
   end
 
