4a6097556f2814aa67340da9ebb95f515fed66b1
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-21 13:00:18 -0600

    added a new model named Page w/ attr title and content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_20_211100) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_21_185924) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -82,6 +82,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_20_211100) do
     t.datetime "updated_at", null: false
     t.integer "status"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
