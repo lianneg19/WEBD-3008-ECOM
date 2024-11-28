b59a7eee09f552fbb70c5262a13bc677ff3b042b
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-27 19:36:51 -0600

    added their own authentication key type for login

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [ :username ]
   has_many :orders
   has_many :comments
   has_one :cart
