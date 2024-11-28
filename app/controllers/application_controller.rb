a8cda3a907576a896012c1a2a9bd593eda1c802b
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-27 23:06:33 -0600

    added method to find or create the customer's cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,25 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :initialize_order
+  helper_method :current_customer
 
   def after_sign_out_path_for(resource_or_scope)
     new_customer_session_path
+  end
+
+  # sources used: https://www.youtube.com/watch?v=SPokmOwiM7E
+  def initialize_order
+    if current_customer
+      @order ||= Order.find_by(id: session[:order_id],
+        customer_id: current_customer.id)
+
+      if @order.nil?
+        @order = Order.create(customer_id: current_customer.id)
+        session[:order_id] = @order.id
+      end
+    else
+      @order = nil
+    end
   end
 end
