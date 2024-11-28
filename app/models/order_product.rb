852dd06936406ad08837e1b46f43c93b4e09fb91
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-28 14:26:34 -0600

    created methods to calcualte the unit and total price

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -2,6 +2,9 @@ class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
+  before_save :set_unit_price
+  before_save :set_total_price
+
   def self.ransackable_attributes(auth_object = nil)
     [ "created_at", "id", "order_id", "product_id", "updated_at", "quantity" ]
   end
@@ -9,4 +12,26 @@ class OrderProduct < ApplicationRecord
   def self.ransackable_associations(auth_object = nil)
     [ "order", "product" ]
   end
+
+  # sources used by: https://www.youtube.com/@HappyCoding
+  def unit_price
+    if persisted?
+      seld[:unit_price]
+    else
+      product.price
+    end
+  end
+
+  def total_price
+    unit_price * quantity
+  end
+
+  private
+    def set_unit_price
+      self[:unit_price] = unit_price
+    end
+
+    def set_total_price
+      self[:tota_price] = quantity & set_unit_price
+    end
 end
