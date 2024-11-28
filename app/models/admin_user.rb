b280f66d884b5f748d8e6e1e8f57dca85dec722d
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-27 19:36:51 -0600

    added their own authentication key type for login

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,9 +2,9 @@ class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [ :email ]
 
   def self.ransackable_attributes(auth_object = nil)
-    [ "created_at", "email", "encrypted_password", "id", "id_value", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at" ]
+    [ "created_at", "email", "username", "encrypted_password", "id", "id_value", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at" ]
   end
 end
