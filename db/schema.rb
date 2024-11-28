ad247f75077177935728cb25d3932138a231b795
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-27 21:29:02 -0600

    reverted the create Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_28_022327) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_22_064151) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -35,13 +35,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_28_022327) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-  end
-
-  create_table "carts", force: :cascade do |t|
-    t.integer "customer_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["customer_id"], name: "index_carts_on_customer_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -119,7 +112,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_28_022327) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "carts", "customers"
   add_foreign_key "comments", "customers"
   add_foreign_key "comments", "products"
   add_foreign_key "order_products", "orders"
