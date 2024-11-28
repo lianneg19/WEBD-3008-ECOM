e8cf4851b2a77abc5c11d817d7b69ca8d684b158
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-27 20:27:08 -0600

    created a Cart model with a reference to Customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_21_204500) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_22_064151) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -104,6 +104,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_21_204500) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "customers"
