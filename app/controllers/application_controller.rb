cb0a3584f32921818830a25eea04d1c2e832e1e5
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-29 02:21:00 -0600

    commented out the initialize order cart method and helper method current customer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,25 +2,24 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :initialize_order_cart
-  helper_method :current_customer
+  # helper_method :current_customer
 
   def after_sign_out_path_for(resource_or_scope)
     new_customer_session_path
   end
 
   # sources used: https://www.youtube.com/watch?v=SPokmOwiM7E
-  def initialize_order_cart
-    if current_customer
-      @order ||= Order.find_by(id: session[:order_id],
-        customer_id: current_customer.id)
+  # def initialize_order_cart
+  #   if current_customer
+  #     @order ||= Order.find_by(id: session[:order_id],
+  #       customer_id: current_customer.id)
 
-      if @order.nil?
-        @order = Order.create(customer_id: current_customer.id)
-        session[:order_id] = @order.id
-      end
-    else
-      @order = nil
-    end
-  end
+  #     if @order.nil?
+  #       @order = Order.create(customer_id: current_customer.id)
+  #       session[:order_id] = @order.id
+  #     end
+  #   else
+  #     @order = nil
+  #   end
+  # end
 end
