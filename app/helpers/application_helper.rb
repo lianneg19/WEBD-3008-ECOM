ae2825e90823c8008b8ea079498e9707865153fa
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-29 02:38:12 -0600

    modified the logic behind current_order method to handle session, order, and customer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 module ApplicationHelper
   def current_order
-    if session[:order_id]
-      Order.find(session[:order_id])
-    else
-      order = Order.create
-      session[:order_id] = order.id
-      order
+    if session[:order_id] # is there order_id in session?
+      order = Order.find(session[:order_id]) # if yes, fetch from db
+    elsif customer_signed_in? # if signed in
+      order = current_customer.orders.find_by(status: 1) ||
+      Order.create(customer: current_customer) # find order with "active" status, OTHERWISE create new order for customer
+    else # if no order_id found, customer not signed in
+      order = Order.create(customer: current_customer) # create new order for current customer
+      session[:order_id] = order.id # store order_id to session
     end
+    order # return order
   end
 end
