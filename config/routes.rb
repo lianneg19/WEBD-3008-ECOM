2d903778c3d143126b6382a89fcfaaeabe6fceca
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-28 20:32:44 -0600

    fixed my cart route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "cart/show"
   devise_for :customers, controllers: {
     registrations: "customers/registrations",
     sessions: "customers/sessions"
@@ -13,6 +12,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :orders
   resources :customers
+  resource :carts, only: [ :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
