2031d4cce743ad62efd7753de6d96c9de575ab07
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-11-28 20:16:50 -0600

    modified the current order method for handling the session order

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
   def current_order
-    if !session[:order_id].nil
-      order.find(session[:order_id])
+    if session[:order_id]
+      Order.find(session[:order_id])
     else
-      Order.new
+      order = Order.create
+      session[:order_id] = order.id
+      order
     end
   end
 end
