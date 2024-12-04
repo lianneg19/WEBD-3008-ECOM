36a846d793e6d6e7aaa9a10017bcf5fef4665cb5
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-12-04 14:56:07 -0600

    added routes for my checkout

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,11 @@ Rails.application.routes.draw do
   resources :orders
   resources :customers
   resource :carts, only: [ :show ]
+  scope "/checkout" do
+    post "create", to: "checkout#create", as: "checkout_create"
+    get "success", to: "checkout#success", as: "checkout_success"
+    get "cancel", to: "checkout#cancel", as: "checkout_cancel"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
