0cc1e8d1261af2c608fca3ba6b6e5af420311f8e
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-12-04 14:49:30 -0600

    generated checkout view & created create, success, and cancel methods for the stripe checkout process

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,2 +1,38 @@
 class CheckoutController < ApplicationController
+  def create
+    order_product_ids = params[:order_product_ids]
+    order_products = OrderProduct.where(id: order_product_ids)
+
+    if order_products.nil?
+      redirect_to root_path
+      return
+    end
+
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: [ "card" ],
+      success_url: checkout_success_url,
+      cancel_url: checkout_cancel_url,
+      mode: "payment",
+      line_items: order_products.map do |order|
+        {
+        price_data: {
+          currency: "cad",
+          product_data: {
+            name: order.product.name,
+            description: order.product.description
+          },
+          unit_amount: (order.product.price * 100).to_i
+        },
+        quantity: 1
+      }
+      end
+    )
+
+    redirect_to @session.url, allow_other_host: true
+  end
+
+  def success
+  end
+  def cancel
+  end
 end
