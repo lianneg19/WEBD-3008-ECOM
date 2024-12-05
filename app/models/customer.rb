835b5c28da88568a616ea0a3f933de2607068241
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-12-04 23:24:14 -0600

    added an association with province

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,6 +6,7 @@ class Customer < ApplicationRecord
   has_many :orders
   has_many :comments
   has_one :cart
+  belongs_to :province
 
   validates :first_name, :last_name, :address, :phone_number, :user_role, presence: true
   enum user_role: { admin: 1, registered_user: 2, visitor: 3 }, _default: 2
